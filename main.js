1d9979a82942f29763a0ce38c87848f4fc76ce47
Saeed Ahmed <phoolisahab@gmail.com>
2023-04-12 06:53:36 +0500

    Add password generated functionality

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-console.log("hey");
-const strengthEls = document.querySelectorAll(".inner_box")
-const passwordDisplayEl = document.querySelector(".generated")
-const generateBtn = document.querySelector(".generate")
+const strengthEls = document.querySelectorAll(".inner_box");
+const passwordDisplayEl = document.querySelector(".generated");
+const generateBtn = document.querySelector(".generate");
 
-
-let passwordStrength = "easy"
-strengthEls.forEach((el) =>{
-    el.addEventListener("click", ()=>{
-        removeActiveClasses()
-        passwordStrength = el.innerHTML.toLowerCase();
-        el.classList.add(`${el.innerHTML.toLocaleLowerCase()}`)
-        passwordDisplayEl.classList.add(`${el.innerHTML.toLowerCase()}`)
-    })
-})
-function removeActiveClasses(){
-    passwordDisplayEl.setAttribute("class", "generated")
-    strengthEls.forEach((el) =>{
-        el.setAttribute("class", "inner_box")
-    })
+let passwordStrength = "easy";
+strengthEls.forEach((el) => {
+  el.addEventListener("click", () => {
+    removeActiveClasses();
+    passwordStrength = el.innerHTML.toLowerCase();
+    el.classList.add(`${el.innerHTML.toLocaleLowerCase()}`);
+    passwordDisplayEl.classList.add(`${el.innerHTML.toLowerCase()}`);
+  });
+});
+function removeActiveClasses() {
+  passwordDisplayEl.setAttribute("class", "generated");
+  strengthEls.forEach((el) => {
+    passwordDisplayEl.innerHTML = "Click Generate"
+    el.setAttribute("class", "inner_box");
+  });
 }
-generateBtn.addEventListener("click", createPassword)
-function createPassword(){
-    if(passwordStrength == "easy"){
-        console.log("easy");
-    }else if(passwordStrength == "medium"){
-        console.log("medium");
-    }else{
-        console.log("hard");
-    }
+generateBtn.addEventListener("click", createPassword);
+function createPassword() {
+  if (passwordStrength == "easy") {
+    let char = "abcdefghijklmnopqrstuvwxyz"
+    generatePassword(char)
+  } else if (passwordStrength == "medium") {
+    let char = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    generatePassword(char)
+  } else {
+    let char = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    generatePassword(char)
+  }
+
+}
+function generatePassword(char){
+  let password = ""
+  function randomNumberGenerator(){
+    let random = Math.floor(Math.random()*char.length)
+    return random
+  }
+  for(let i = 0; i < 10; i++){
+    let randomIndex = randomNumberGenerator();
+    password += char[randomIndex]
+  }
+  passwordDisplayEl.innerHTML = password
 }
